16bb6be362236375e1c921dcfc57a90fffca5042
Aiden-D0 <aidenduong118@gmail.com>
2025-07-27 22:49:41 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,14 @@
+// Your Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAbgrezF6aYC1LTcVXsY7CFnJU6ex29-P8",
+  authDomain: "webchatapp-4fbaf.firebaseapp.com",
+  projectId: "webchatapp-4fbaf",
+  storageBucket: "webchatapp-4fbaf.firebasestorage.app",
+  messagingSenderId: "924513856351",
+  appId: "1:924513856351:web:a8cf2542dbdde066f624ea",
+  measurementId: "G-YYYRRQ0CYQ"
+};
+
+// Initialize Firebase using compat (for CDN setup)
+firebase.initializeApp(firebaseConfig);
 
