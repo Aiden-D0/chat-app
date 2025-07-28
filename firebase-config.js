6cf0f590bf6a23632d6c8da779df8de91a211973
Aiden-D0 <aidenduong118@gmail.com>
2025-07-28 10:37:16 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,12 @@
 const firebaseConfig = {
   apiKey: "AIzaSyAbgrezF6aYC1LTcVXsY7CFnJU6ex29-P8",
   authDomain: "webchatapp-4fbaf.firebaseapp.com",
-  databaseURL: "https://webchatapp-4fbaf-default-rtdb.asia-southeast1.firebasedatabase.app",
+  databaseURL: "https://webchatapp-4fbaf-default-rtdb.firebaseio.com", // ✅ THIS IS THE FIX
   projectId: "webchatapp-4fbaf",
   storageBucket: "webchatapp-4fbaf.appspot.com",
   messagingSenderId: "924513856351",
-  appId: "1:924513856351:web:a8cf2542dbdde066f624ea"
+  appId: "1:924513856351:web:a8cf2542dbdde066f624ea",
+  measurementId: "G-YYYRRQ0CYQ"
 };
 
 firebase.initializeApp(firebaseConfig);
